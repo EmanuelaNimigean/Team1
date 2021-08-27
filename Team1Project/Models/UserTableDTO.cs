fc538adddd591ffa8ad75d8981cb0b11e32f6aa9
Tudor Andrei Pop <tudor_fb@yahoo.com>
2021-08-27 13:57:51 +0300

    Role assining dropdown in user, Added UserTableDTO

---
+++ Team1Project/Models/UserTableDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Team1Project.Models
+{
+    public class UserTableDTO
+    {
+        public UserTableDTO(List<UserDTO> users, List<IdentityRole> roles)
+        {
+            this.Users = users;
+            this.Roles = roles;
+        }
+        public List<UserDTO> Users { get; set; }
+        public List<IdentityRole> Roles{ get; set; }
+    }
+}
